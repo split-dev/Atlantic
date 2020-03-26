938daf4b77464fb43e3cc944f15e1429e860047d
vlad <vkravchenko@splitdev.pro>
2020-03-26 14:05:09 +0200

    foundation config complete | symbols added | added util classes | footer desktop

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,8 @@
 export default {
   init() {
-
+    // document.querySelectorAll('.btn').forEach(btn => btn.append(
+    //
+    // ));
   },
 
   // JavaScript to be fired on all pages, after page specific JS is fired
