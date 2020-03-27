31b77a317a3cb678b6a58ab097aecd78f752dc81
vlad <vkravchenko@splitdev.pro>
2020-03-27 16:48:28 +0200

    polifill foreach  add

--- resources/assets/scripts/util/polyfills.js
+++ resources/assets/scripts/util/polyfills.js
@@ -78,3 +78,12 @@ if (typeof Object.assign != 'function') {
     return target;
   };
 }
+
+if ('NodeList' in window && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
