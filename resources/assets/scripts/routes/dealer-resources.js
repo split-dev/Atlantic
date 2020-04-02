3c9bbfb7e704e1797c2e459a4d806decd5d3d4d0
vlad <vkravchenko@splitdev.pro>
2020-04-02 12:03:17 +0300

    dealer resources page: image-block customized if it's after header (role Page Intro) & added tabs (JS only)

---
+++ resources/assets/scripts/routes/dealer-resources.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import { Foundation, Tabs } from 'foundation-sites/dist/js/plugins/foundation.tabs'
+
+export default {
+  init() {
+    // eslint-disable-next-line no-unused-vars
+    var tabs = new Foundation.Tabs($('[data-tabs]'));
+  },
+  // JavaScript to be fired on all pages, after page specific JS is fired
+  finalize() {
+  },
+};
